747db72b5d9ec7e633a30d071bfb20b2f6486c4c
Anthony Palasse <anthony@you2you.fr>
2017-04-19 11:59:49 +0200

    update documentation Header

--- api_project.js
+++ api_project.js
@@ -1,17 +1,1 @@
-define({
-  "name": "you2you-api",
-  "version": "0.0.1",
-  "description": "You2You Development API",
-  "header": {
-    "title": "Information",
-    "content": "<p>Merci de créér un compte partenaire sur www.partenaires.you2you.fr afin de recupérer vos API Keys.</p>\n<p>Pour nous joindre pour tout problèmes/suggestions/remarques via l'adresse email: dev [@] you2you.fr</p>\n<p>Merci de suivre cette procédure pour vos emails afin qu'ils soient traités dans les meilleurs délais.\n[OBJET MAIL]: SUPPORT API - NOM SOCIETE\nEmail du compte\nAPI Key\nRequête HTTP + contenu JSON</p>\n"
-  },
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2016-12-01T14:55:10.795Z",
-    "url": "http://apidocjs.com",
-    "version": "0.16.1"
-  }
-});
+define({  "name": "you2you-api",  "version": "0.0.1",  "description": "You2You Development API",  "header": {    "title": "Information",    "content": "<p>Merci de créér un compte partenaire sur <a href=\"https://partenaires.you2you.fr/account/signup\">https://partenaires.you2you.fr/account/signup</a> afin de recupérer vos API Keys.</p>\n<p>Pour nous joindre pour tout problèmes/suggestions/remarques via l'adresse email: dev [@] you2you.fr</p>\n<p>Merci de suivre cette procédure pour vos emails afin qu'ils soient traités dans les meilleurs délais.\n[OBJET MAIL]: SUPPORT API - NOM SOCIETE\nEmail du compte\nAPI Key\nRequête HTTP + contenu JSON</p>\n"  },  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-04-19T09:57:57.389Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
